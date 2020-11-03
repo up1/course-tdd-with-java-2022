cd657124c18ab25bec41ba241c428f2737d7114f
up1 <somkiat.p@gmail.com>
2020-11-03 22:45:46 +0700

    Demo jacoco with Gradle

---
+++ demo-spring-boot-gradle/README.md
@@ -0,0 +1,19 @@
+# How to run ?
+
+## Run test and coverage with jacoco
+```
+$gradlew clean test
+```
+
+## Run coverage report with Jacoco
+```
+$gradlew jacocoTestReport
+```
+
+* HTML report in `build/reports/jacoco/test/html/index.html`
+* Jacoco result in `build/jacoco/test.exec`
+
+## Start Development server
+```
+$mvnw bootRun
+```
---
+++ demo-spring-boot-gradle/src/main/java/com/example/demo/DemoSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootGradleApplication.class, args);
+	}
+
+}
