e5fd9f13a50d50d7f60742ea6fa1444c6ca79b0b
up1 <somkiat.p@gmail.com>
2020-11-04 14:18:20 +0700

    Add test with wiremock

--- demo02/src/test/java/APITest.java
+++ demo02/src/test/java/APITest.java
@@ -1,8 +1,11 @@
 import com.github.tomakehurst.wiremock.WireMockServer;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.*;
 
 public class APITest {
     WireMockServer wireMockServer;
@@ -23,8 +26,17 @@ public class APITest {
         wireMockServer.stubFor(get(urlEqualTo("/users/1"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
-                                .withStatus(200)
-                                .withBodyFile("json/hello.json")));
+                                .withStatus(200)));
     }
+
+    @Test
+    public void working_with_wiremock_200() {
+        given().
+                when().
+                get("http://localhost:8090/users/1").
+                then().
+                assertThat().statusCode(200);
+    }
+
 
 }
