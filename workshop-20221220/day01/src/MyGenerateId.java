f6e9eb0f24c950183911ee852882b26e97d863df
up1 <somkiat.p@gmail.com>
2022-12-20 15:06:35 +0700

    Manage dependency in method override

--- workshop-20221220/day01/src/MyGenerateId.java
+++ workshop-20221220/day01/src/MyGenerateId.java
@@ -2,8 +2,9 @@ import java.util.Random;
 
 public class MyGenerateId {
 
+    Random random;
+
     public String getId() {
-        Random random = new Random();
         int number = random.nextInt(10);
         String result = "XXX" + number;
         return result;
--- workshop-20221220/day01/src/MyGenerateIdTest.java
+++ workshop-20221220/day01/src/MyGenerateIdTest.java
@@ -1,12 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MyGenerateIdTest {
+class MyRandom6 extends Random{
+    @Override
+    public int nextInt(int bound) {
+        return 6;
+    }
+}
 
+class MyGenerateIdTest {
     @Test
     void getIdWith6() {
+        Random random = new MyRandom6();
         MyGenerateId m = new MyGenerateId();
+        // Field,property injection
+        m.random = random;
         assertEquals("XXX6", m.getId());
     }
 }
