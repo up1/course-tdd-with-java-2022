af0c483df6725d1b4fabc38188d2a9ce40d42c1f
up1 <somkiat.p@gmail.com>
2022-12-20 13:36:23 +0700

    Data-driven testing with junit5

--- workshop-20221220/day01/src/Workshop1Test.java
+++ workshop-20221220/day01/src/Workshop1Test.java
@@ -1,21 +1,50 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Workshop1Test {
+    @ParameterizedTest
+    @CsvSource({
+            "'[1,5]', '1,2,3,4,5'",
+            "'(1,5]', '2,3,4,5'",
+            "'[1,5)', '1,2,3,4'"
+    })
+    public void flow(String input, String expectedResult) {
+        Workshop1 workshop1 = new Workshop1(input);
+        String actualResult = workshop1.getResult();
+        assertEquals(expectedResult, actualResult);
+    }
 
     @Test
     @DisplayName("[1,5] => 1,2,3,4,5")
-    public void case05(){
+    public void case05() {
         Workshop1 workshop1 = new Workshop1("[1,5]");
         String actualResult = workshop1.getResult();
         assertEquals("1,2,3,4,5", actualResult);
     }
 
     @Test
+    @DisplayName("(1,5] => 2,3,4,5")
+    public void case07() {
+        Workshop1 workshop1 = new Workshop1("(1,5]");
+        String actualResult = workshop1.getResult();
+        assertEquals("2,3,4,5", actualResult);
+    }
+
+    @Test
+    @DisplayName("[1,5) => 1,2,3,4")
+    public void case06() {
+        Workshop1 workshop1 = new Workshop1("[1,5)");
+        String actualResult = workshop1.getResult();
+        assertEquals("1,2,3,4", actualResult);
+    }
+
+    @Test
     @DisplayName("start with include [1,5] => 1")
-    public void case03(){
+    public void case03() {
         Workshop1 workshop1 = new Workshop1("[1,5]");
         int actualResult = workshop1.getStart();
         assertEquals(1, actualResult);
@@ -23,7 +52,7 @@ public class Workshop1Test {
 
     @Test
     @DisplayName("start with excluse (1,5] => 2")
-    public void case04(){
+    public void case04() {
         Workshop1 workshop1 = new Workshop1("(1,5]");
         int actualResult = workshop1.getStart();
         assertEquals(2, actualResult);
@@ -31,7 +60,7 @@ public class Workshop1Test {
 
     @Test
     @DisplayName("start with include [1,5] => true")
-    public void case01(){
+    public void case01() {
         Workshop1 workshop1 = new Workshop1("[1,5]");
         boolean actualResult
                 = workshop1.startWithInclude();
@@ -40,7 +69,7 @@ public class Workshop1Test {
 
     @Test
     @DisplayName("start with exclude (1,5] => false")
-    public void case02(){
+    public void case02() {
         Workshop1 workshop1 = new Workshop1("(1,5]");
         boolean actualResult
                 = workshop1.startWithInclude();
