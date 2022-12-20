d6885e01fa46f8eace394802e97c1baa372eae1b
up1 <somkiat.p@gmail.com>
2022-12-20 11:43:46 +0700

    Refactor with magic number

--- workshop-20221220/day01/src/Workshop1.java
+++ workshop-20221220/day01/src/Workshop1.java
@@ -1,4 +1,5 @@
 public class Workshop1 {
+    public static final int ASCII_VALUE_OF_ZERO = 48;
     private final String input;
 
     public Workshop1(String input) {
@@ -10,6 +11,7 @@ public class Workshop1 {
     }
 
     public int getStart() {
-        return 0;
+        // 48 = ASCII code ของ 0
+        return input.charAt(1) - ASCII_VALUE_OF_ZERO;
     }
 }
