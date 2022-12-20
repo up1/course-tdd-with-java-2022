18c2079e6252746b9a6ffa489f3dfc999fb170b3
up1 <somkiat.p@gmail.com>
2022-12-20 11:22:41 +0700

    Working with 1,2 case

--- workshop-20221220/day01/src/Workshop1.java
+++ workshop-20221220/day01/src/Workshop1.java
@@ -1,11 +1,11 @@
 public class Workshop1 {
-    private String input;
+    private final String input;
 
     public Workshop1(String input) {
         this.input = input;
     }
 
     public boolean startWithInclude() {
-        return false;
+        return input.startsWith("[");
     }
 }
--- workshop-20221220/day01/src/Workshop1Test.java
+++ workshop-20221220/day01/src/Workshop1Test.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Workshop1Test {
@@ -8,8 +10,18 @@ public class Workshop1Test {
     @DisplayName("start with include [1,5] => true")
     public void case01(){
         Workshop1 workshop1 = new Workshop1("[1,5]");
-        boolean actualResult = workshop1.startWithInclude();
+        boolean actualResult
+                = workshop1.startWithInclude();
         assertTrue(actualResult);
+    }
+
+    @Test
+    @DisplayName("start with excluse (1,5] => false")
+    public void case02(){
+        Workshop1 workshop1 = new Workshop1("(1,5]");
+        boolean actualResult
+                = workshop1.startWithInclude();
+        assertFalse(actualResult);
     }
 
 }
