ad22620534858a8db5613c97ff896c2f9cb2c67f
up1 <somkiat.p@gmail.com>
2022-12-21 10:40:08 +0700

    Reduce duplication code

--- workshop-20221220/day02/src/main/java/org/example/TennisGame2.java
+++ workshop-20221220/day02/src/main/java/org/example/TennisGame2.java
@@ -17,63 +17,36 @@ public class TennisGame2 implements TennisGame {
     }
 
     public String getScore() {
+        String[] scoreNames
+                = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
         String score = "";
         if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
+            score = scoreNames[P1point];
             score += "-All";
         }
         if (P1point == P2point && P1point >= 3)
             score = "Deuce";
 
         if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
+            if(P1point <= 3 ){
+                P1res = scoreNames[P1point];
+            }
+            P2res = scoreNames[P2point];
             score = P1res + "-" + P2res;
         }
         if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
+            if(P2point <= 3 ){
+                P2res = scoreNames[P2point];
+            }
+            P1res = scoreNames[P1point];
             score = P1res + "-" + P2res;
         }
 
         if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
+            score = scoreNames[P1point] + "-" + scoreNames[P2point];
         }
         if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
+            score = scoreNames[P1point] + "-" + scoreNames[P2point];
         }
 
         if (P1point > P2point && P2point >= 3) {
