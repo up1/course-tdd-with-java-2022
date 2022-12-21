074b290022a42539b5200a2a6f2562000fdb3a6b
up1 <somkiat.p@gmail.com>
2022-12-21 14:09:36 +0700

    Fizzbuzz kata

--- workshop-20221220/day02/src/main/java/org/example/Fizzbuzz.java
+++ workshop-20221220/day02/src/main/java/org/example/Fizzbuzz.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Fizzbuzz {
     public String call(int input) {
+        if (input % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+        if (input % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(input);
     }
 }
--- workshop-20221220/day02/src/test/java/org/example/FizzbuzzTest.java
+++ workshop-20221220/day02/src/test/java/org/example/FizzbuzzTest.java
@@ -15,5 +15,28 @@ class FizzbuzzTest {
     public void case02(){
         Fizzbuzz fizzbuzz = new Fizzbuzz();
         assertEquals("2", fizzbuzz.call(2));
+        assertEquals("4", fizzbuzz.call(4));
+    }
+
+    @Test
+    public void case03(){
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        assertEquals("Fizz", fizzbuzz.call(3));
+        assertEquals("Fizz", fizzbuzz.call(6));
+        assertEquals("Fizz", fizzbuzz.call(9));
+    }
+
+    @Test
+    public void case04(){
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        assertEquals("Buzz", fizzbuzz.call(5));
+        assertEquals("Buzz", fizzbuzz.call(10));
+    }
+
+    @Test
+    public void case05(){
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        assertEquals("FizzBuzz", fizzbuzz.call(15));
+        assertEquals("FizzBuzz", fizzbuzz.call(30));
     }
 }
